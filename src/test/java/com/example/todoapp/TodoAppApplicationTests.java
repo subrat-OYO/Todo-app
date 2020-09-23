e41b710001c5798089f6899593a31a81da4d22f5
subrat-OYO <subrat.sahu@oyorooms.com>
2020-09-23 19:30:22 +0530

    testing home commit again

--- src/test/java/com/example/todoapp/TodoAppApplicationTests.java
+++ src/test/java/com/example/todoapp/TodoAppApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.todoapp;
 
+import com.example.todoapp.entity.TodoEntity;
+import com.example.todoapp.repository.TodoRepository;
+import com.example.todoapp.service.TodoService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(MockitoJUnitRunner.class)
 class TodoAppApplicationTests {
+
+    @Mock
+    TodoRepository repo;
+
+    @InjectMocks
+    TodoService service;
 
     @Test
     void contextLoads() {
+
+        //assertEquals("task deleted",service.delete(1));
     }
 
 }
